55879e9f45a4b2f81875608de3b3a8b374a171de
Ivan Hancko <ivanhancko@yahoo.com>
2023-07-11 09:30:29 +0200

    add sing up and log in forms

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/home/home.component';
+import { AboutComponent } from './core/about/about.component';
+import { SingupComponent } from './singup/singup.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent },
+  {path: 'about', component: AboutComponent },
+  {path: 'singup', component: SingupComponent },
+  {path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
